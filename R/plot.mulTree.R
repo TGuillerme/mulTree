b515b59971af0a6b11efa22be1fb5a9efb5e3fb1
TGuillerme <guillert@tcd.ie>
2014-08-13 17:31:35 +0100

    removed silly options

--- R/plot.mulTree.R
+++ R/plot.mulTree.R
@@ -6,17 +6,21 @@
 ##########################
 #SYNTAX :
 #<mulTree.mcmc> a mcmc chain written by the mulTree function. Can be either a unique file or a chain name referring to multiple files. Use read.mulTree() to properly load the chains
-
-
+#<CI> the credibility interval (can be more than one value)
+#<average> the central tendency of the distribution to plot. Can be either 'mode', 'median' or 'mean' (default="mode")
+#<horizontal> whether to plot the boxplots horizontally or not (default=TRUE)
+#<terms> a list of terms, if NULL, the terms are extracted from mulTree.mcmc (default=NULL)
+#<colour> any colour or list of colour for the plot, if NULL colour is set to greyscale (default=NULL)
+#<coeff.lim> the estimate coefficient range, if NULL, the range is set to the extreme values of mulTree.mcmc +/- 10%
 
 
 #<...> any additional argument to be passed to plot() function
 #<horizontal> whether to plot the boxplots horizontally or not (default=TRUE)
-#<add.table> a table.mulTreea.frame with the row names corresponding to the names of the "hdr.mcmc" to add to the plot (default=NULL). If add.table is not NULL, horizontal is set to TRUE.
+#<add.table> a mulTree.mcmca.frame with the row names corresponding to the names of the "hdr.mcmc" to add to the plot (default=NULL). If add.table is not NULL, horizontal is set to TRUE.
 #<LaTeX> whether to print the latex code
 ##########################
 #----
-#guillert(at)tcd.ie - 12/08/2014
+#guillert(at)tcd.ie - 13/08/2014
 ##########################
 #Requirements:
 #-R 3
@@ -26,7 +30,7 @@
 ##########################
 
 
-plot.mulTree<-function(mulTree.mcmc, horizontal=FALSE,  CI = c(95, 75, 50), xlabels = NULL, ylabels= NULL, colour = NULL, prn = FALSE, leg = FALSE, ct = "mode",ylims=NULL)
+plot.mulTree<-function(mulTree.mcmc, CI=c(95, 75, 50), average="mode", horizontal=FALSE, terms=NULL, colour=NULL, coeff.lim=NULL)
 {
 #HEADER
     require(MCMCglmm)
@@ -34,13 +38,35 @@ plot.mulTree<-function(mulTree.mcmc, horizontal=FALSE,  CI = c(95, 75, 50), xlab
 
 #DATA
     #mulTree.mcmc
-    if (class(mulTree.mcmc) != "table.mulTree") {
-        table.mulTree<-table.mulTree(mulTree.mcmc)
+    if(class(mulTree.mcmc) != 'mulTree') {
+        stop(as.character(substitute(mulTree.mcmc))," must be a 'mulTree' object.\nUse read.mulTree() function.", call.=FALSE)
+    } else {
+        #rebuild mulTree.mcmc as a data.frame
+        class(mulTree.mcmc)<-"data.frame"
     }
 
-    #colout
-    if (is.null(colour)) {
-        colour=gray((9:1)/10)
+    #CI
+    if (class(CI) != 'numeric') {
+        stop("Credibility interval must be between 0 and 100.", call.=FALSE)
+    }
+    if (any(CI < 0)) {
+        stop("Credibility interval must be between 0 and 100.", call.=FALSE)
+    } else {
+        if (any(CI > 100)) {
+            stop("Credibility interval must be between 0 and 100.", call.=FALSE)
+        }
+    }
+
+    #average
+    first.moment<-c("mode", "median", "mean")
+    if (class(average) != 'character') {
+        stop("\"average\" must be either \"mode\", \"median\" or \"mean\".", call.=FALSE)
+    } else {
+        if (any(average == first.moment)) {
+            ok<-"ok"
+        } else {
+            stop("\"average\" must be either \"mode\", \"median\" or \"mean\".", call.=FALSE)
+        }       
     }
 
     #horizontal
@@ -48,45 +74,60 @@ plot.mulTree<-function(mulTree.mcmc, horizontal=FALSE,  CI = c(95, 75, 50), xlab
         stop('"horizontal" must be logical.')
     }
 
+    #colour
+    if (is.null(colour)) {
+        colour=gray((9:1)/10)
+    }
+
+    #terms
+    if(is.null(terms)) {
+        terms=as.character(names(mulTree.mcmc))
+    }
+
+    #coeff.lim
+    if (is.null(coeff.lim)) {
+        coeff.lim<-c(min(mulTree.mcmc) - 0.1*min(mulTree.mcmc), max(mulTree.mcmc) + 0.1*(max(mulTree.mcmc)))
+    } else {
+        if(class(coeff.lim) != 'numeric') {
+            stop("\"coeff.lim\" must be numeric.", call.=FALSE)
+        } else {
+            if(length(coeff.lim) != 2) {
+                stop("\"coeff.lim\" must be a list of two elements.", call.=FALSE))
+            }
+        }
+    }
+
     #add.table
 
 #FUNCTION
 
     #Density Plot function (from densityplot.R by Andrew Jackson - a.jackson@tcd.ie)
-    FUN.densityplot <- function (table.mulTree, CI , xlabels , ylabels, colour , prn , leg , ct ,ylims)
+    FUN.densityplot <- function (mulTree.mcmc, CI, average, terms, colour, coeff.lim)
     {
 
     #x spacement
     xspc<-0.5
-    n <- ncol(table.mulTree)
-
-    if (is.null(ylabels)){ylabels="value"}
+    n <- ncol(mulTree.mcmc)
         
     # Set up the plot
-    if (is.null(ylims)){ylims<-c(min(table.mulTree) - 0.1*min(table.mulTree), max(table.mulTree) + 0.1*(max(table.mulTree)))}
+    if (is.null(coeff.lim)){coeff.lim<-c(min(mulTree.mcmc) - 0.1*min(mulTree.mcmc), max(mulTree.mcmc) + 0.1*(max(mulTree.mcmc)))}
 
-    plot(1,1, xlab = "Source", ylab = ylabels, main = paste("","", sep = ""),
-                xlim = c(1 - xspc, n + xspc),
-                ylim = ylims, type = "n",
-                xaxt = "n")
-            if (is.null(xlabels)) {
-                axis(side = 1, at = 1:n,
-                    labels = (as.character(names(table.mulTree))))
-            } else {
-                axis(side = 1, at = 1:n,
-                    labels = (xlabels))
-            }
+    plot(1,1 , xlab="", xlim = c(1 - xspc, n + xspc), ylim = coeff.lim, type = "n", xaxt = "n")
+    #x or y label may change
+
+    axis(side = 1, at = 1:n, labels = (terms))
+
 
 
 
     colours <- rep(colour, 5)
     for (j in 1:n) {
-            temp <- hdr(table.mulTree[, j], CI, h = bw.nrd0(table.mulTree[,j]))
+            temp <- hdr(mulTree.mcmc[, j], CI, h = bw.nrd0(mulTree.mcmc[,j]))
             line_widths <- seq(2, 20, by = 4)
             bwd <- c(0.1, 0.15, 0.2, 0.25, 0.3)
-            if (prn == TRUE) {
-                cat(paste("Probability values for Column", j, "\n"))
-            }
+            #if (prn == TRUE) {
+            #    cat(paste("Probability values for Column", j, "\n"))
+            #}
             for (k in 1:length(CI)) {
                 temp2 <- temp$hdr[k, ]
                 type="boxes"
@@ -95,25 +136,25 @@ plot.mulTree<-function(mulTree.mcmc, horizontal=FALSE,  CI = c(95, 75, 50), xlab
                       c(min(temp2[!is.na(temp2)]), max(temp2[!is.na(temp2)]), 
                       max(temp2[!is.na(temp2)]), min(temp2[!is.na(temp2)])),
                       col = colours[k])
-                    if (ct == "mode") {points(j,temp$mode,pch=19)}
-                    if (ct == "mean") {points(j,mean(table.mulTree[,j]),pch=19)}
-                    if (ct == "median") {points(j,median(table.mulTree[,j]),pch=19)}
+                    if (average == "mode") {points(j,temp$mode,pch=19)}
+                    if (average == "mean") {points(j,mean(mulTree.mcmc[,j]),pch=19)}
+                    if (average == "median") {points(j,median(mulTree.mcmc[,j]),pch=19)}
 
                 }
-                if (prn == TRUE) {
-                    cat(paste("\t", CI[k], "% lower =", format(max(min(temp2[!is.na(temp2)]),
-                      0), digits = 2, scientific = FALSE), "upper =",
-                      format(min(max(temp2[!is.na(temp2)]), 1), digits = 2,
-                        scientific = FALSE), "\n"))
-                }
+                #if (prn == TRUE) {
+                #    cat(paste("\t", CI[k], "% lower =", format(max(min(temp2[!is.na(temp2)]),
+                #      0), digits = 2, scientific = FALSE), "upper =",
+                #      format(min(max(temp2[!is.na(temp2)]), 1), digits = 2,
+                #        scientific = FALSE), "\n"))
+                #}
             } # close the loop across CI
-        } # close the loop across teh columns in table.mulTree
+        } # close the loop across teh columns in mulTree.mcmc
     }
 
 
 #PLOTTING THE MCMCglmm RESULTS
 
-    FUN.densityplot(table.mulTree, CI , xlabels , ylabels, colour , prn , leg , ct ,ylims)
+    FUN.densityplot(mulTree.mcmc, CI, average, terms, colour, coeff.lim)
 #OUTPUT
 
 #End
