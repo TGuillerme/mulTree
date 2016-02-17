2ca0b6961f984ddc4247892201b5ea3d22b9e308
Thomas Guillerme <guillert@tcd.ie>
2016-02-17 15:28:52 +0000

    Testing scripts cleaned

--- tests/testthat/test-mulTree.R
+++ tests/testthat/test-mulTree.R
@@ -3,13 +3,21 @@ context("mulTree")
 
 # Testing select.tip.labels
 test_that("read.key works - scan option deactivated!", {
-    #Message ()
-    expect_message(read.key("message1", scan=FALSE))
-    expect_message(read.key("message1", "message2", scan=FALSE))
+    #Message
+    expect_message(
+    	read.key("message1", scan=FALSE)
+    	)
+    expect_message(
+    	read.key("message1", "message2", scan=FALSE)
+    	)
     #Too many messages
-    expect_error(read.key("message1", "message2", "message3", scan=FALSE))
+    expect_error(
+    	read.key("message1", "message2", "message3", scan=FALSE)
+    	)
     #Not enough messages
-    expect_error(read.key(scan=FALSE))
+    expect_error(
+    	read.key(scan=FALSE)
+    	)
 })
 
 # Dummy data for testing
@@ -26,25 +34,41 @@ priors <- list(R = list(V = 1/2, nu = 0.002), G = list(G1 = list(V = 1/2, nu = 0
 test_that("lapply.MCMCglmm works", {
     #Errors
     # tree is not a tree (multiPhylo)
-    expect_error(lapply.MCMCglmm(tree[[1]], mulTree.data, formula, priors, parameters), warn=FALSE)
+    expect_error(
+    	lapply.MCMCglmm(tree[[1]], mulTree.data, formula, priors, parameters), warn=FALSE
+    	)
     # mulTree.data is not mulTree
-    expect_error(lapply.MCMCglmm(1, data, formula, priors, parameters), warn=FALSE)
+    expect_error(
+    	lapply.MCMCglmm(1, data, formula, priors, parameters), warn=FALSE
+    	)
     # formula is not a formula
-    expect_error(lapply.MCMCglmm(1, mulTree.data, formula="bla", priors, parameters), warn=FALSE)
+    expect_error(
+    	lapply.MCMCglmm(1, mulTree.data, formula="bla", priors, parameters), warn=FALSE
+    	)
     # priors' not a list
-    expect_error(lapply.MCMCglmm(1, mulTree.data, formula, prior=1, parameters), warn=FALSE)
+    expect_error(
+    	lapply.MCMCglmm(1, mulTree.data, formula, prior=1, parameters), warn=FALSE
+    	)
     # parameters is not a vector
-    expect_error(lapply.MCMCglmm(1, mulTree.data, formula, priors, parameters=1), warn=FALSE)
+    expect_error(
+    	lapply.MCMCglmm(1, mulTree.data, formula, priors, parameters=1), warn=FALSE
+    	)
     # wrong additional argument
-    expect_error(lapply.MCMCglmm(1, mulTree.data, formula, priors, parameters, some_arg="whatever"), warn=FALSE)
+    expect_error(
+    	lapply.MCMCglmm(1, mulTree.data, formula, priors, parameters, some_arg="whatever"), warn=FALSE
+    	)
     
     # When no errors, outputs a MCMCglmm object
     set.seed(1)
     test <- lapply.MCMCglmm(1, mulTree.data, formula, priors, parameters, warn=FALSE)
     # Output is MCMCglmm
-    expect_is(test, "MCMCglmm")
+    expect_is(
+    	test, "MCMCglmm"
+    	)
     # MCMCglmm is of standard length
-    expect_equal(length(test), 18)
+    expect_equal(
+    	length(test), 18
+    	)
 })
 
 # Creating two dummy chains
@@ -55,29 +79,47 @@ model_tree1_chain2 <- lapply.MCMCglmm(1, mulTree.data, formula, priors, paramete
 test_that("convergence.test works", {
     # Errors
     # Not a list
-    expect_error(convergence.test(model_tree1_chain1))
+    expect_error(
+    	convergence.test(model_tree1_chain1)
+    	)
     # Not enough chains
-    expect_error(convergence.test(list(model_tree1_chain1)))
+    expect_error(
+    	convergence.test(list(model_tree1_chain1))
+    	)
     # Missing arguments (error + message)
-    expect_error(expect_message(convergence.test("bla")))
+    expect_error(
+    	expect_message(convergence.test("bla"))
+    	)
     # Convergence works
     test <- convergence.test(list(model_tree1_chain1, model_tree1_chain2))
     # Output is gelman.diag
-    expect_is(test, "gelman.diag")
+    expect_is(
+    	test, "gelman.diag"
+    	)
     # gelman.diag is standard format
-    expect_equal(length(test), 2)
+    expect_equal(
+    	length(test), 2
+    	)
     # psrf is standard matrix
-    expect_is(test$psrf, "matrix")
+    expect_is(
+    	test$psrf, "matrix"
+    	)
 })
 
 #Testing ESS lapply (structure similar to convergence.test)
 test_that("ESS.lapply works", {
     # Errors
     # Something silly
-    expect_error(ESS.lapply(3, 1))
+    expect_error(
+    	ESS.lapply(3, 1)
+    	)
     # ESS is a numeric vector (length 2)
-    expect_is(ESS.lapply(model_tree1_chain1), "numeric")
-    expect_equal(names(ESS.lapply(model_tree1_chain1)), c("(Intercept)","var2"))
+    expect_is(
+    	ESS.lapply(model_tree1_chain1), "numeric"
+    	)
+    expect_equal(
+    	names(ESS.lapply(model_tree1_chain1)), c("(Intercept)","var2")
+    	)
 })
 
 
@@ -85,15 +127,25 @@ test_that("ESS.lapply works", {
 test_that("Quick mulTree example works", {
     #Errors
     #Not mulTree format
-    expect_error(mulTree(data, formula = var1 ~ var2, parameters = c(10000, 10, 1000), chains = 2, prior = priors, output = "quick_example", convergence = 1.1, ESS = 100))
+    expect_error(
+    	mulTree(data, formula = var1 ~ var2, parameters = c(10000, 10, 1000), chains = 2, prior = priors, output = "quick_example", convergence = 1.1, ESS = 100)
+    	)
     #Not matching formula
-    #expect_error(mulTree(mulTree.data, formula = var1 ~ var3, parameters = c(10000, 10, 1000), chains = 2, prior = priors, output = "quick_example", convergence = 1.1, ESS = 100))
+#    expect_error(
+#    	mulTree(mulTree.data, formula = var1 ~ var3, parameters = c(10000, 10, 1000), chains = 2, prior = priors, output = "quick_example", convergence = 1.1, ESS = 100)
+#		)
     #Not enough parameters
-    expect_error(mulTree(mulTree.data, formula = var1 ~ var2, parameters = c(1,1), chains = 2, prior = priors, output = "quick_example", convergence = 1.1, ESS = 100))
+    expect_error(
+    	mulTree(mulTree.data, formula = var1 ~ var2, parameters = c(1,1), chains = 2, prior = priors, output = "quick_example", convergence = 1.1, ESS = 100)
+    	)
     #Chains are not numeric
-    expect_error(mulTree(mulTree.data, formula = var1 ~ var2, parameters = c(10000, 10, 1000), chains = "plenty", prior = priors, output = "quick_example", convergence = 1.1, ESS = 100))
+    expect_error(
+    	mulTree(mulTree.data, formula = var1 ~ var2, parameters = c(10000, 10, 1000), chains = "plenty", prior = priors, output = "quick_example", convergence = 1.1, ESS = 100)
+    	)
     #Priors are not a lits
-    expect_error(mulTree(mulTree.data, formula = var1 ~ var2, parameters = c(10000, 10, 1000), chains = 2, prior = 1, output = "quick_example", convergence = 1.1, ESS = 100))
+    expect_error(
+    	mulTree(mulTree.data, formula = var1 ~ var2, parameters = c(10000, 10, 1000), chains = 2, prior = 1, output = "quick_example", convergence = 1.1, ESS = 100)
+    	)
 
     #
     # Does not run on Travis!
@@ -103,29 +155,60 @@ test_that("Quick mulTree example works", {
     # set.seed(1)
     # mulTree_test1 <- system.time(mulTree(mulTree.data, formula = var1 ~ var2, parameters = c(100, 10, 10), chains = 2, prior = priors, output = "mulTree_testing", verbose = FALSE))
     # #Generates 9 files
-    # expect_equal(length(list.files(pattern = "mulTree_testing")), 9)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree1_chain")), 2)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree2_chain")), 2)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree3_chain")), 2)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree1_conv")), 1)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree2_conv")), 1)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree3_conv")), 1)
+    # expect_equal(
+ 	#  	length(list.files(pattern = "mulTree_testing")), 9
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree1_chain")), 2
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree2_chain")), 2
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree3_chain")), 2
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree1_conv")), 1
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree2_conv")), 1
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree3_conv")), 1
+    #	)
     # #File remove successful
-    # expect_true(all(file.remove(list.files(pattern = "mulTree_testing"))))
+    # expect_true(all(file.remove(list.files(pattern = "mulTree_testing")))
+    #	)
 
     # #Second example (parallel) works
     # set.seed(1)
     # mulTree_test2 <- system.time(mulTree(mulTree.data, formula = var1 ~ var2, parameters = c(100, 10, 10), chains = 2, prior = priors, output = "mulTree_testing", parallel = "SOCK", verbose = FALSE))
     # #Generates 9 files
-    # expect_equal(length(list.files(pattern = "mulTree_testing")), 9)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree1_chain")), 2)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree2_chain")), 2)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree3_chain")), 2)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree1_conv")), 1)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree2_conv")), 1)
-    # expect_equal(length(list.files(pattern = "mulTree_testing-tree3_conv")), 1)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing")), 9
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree1_chain")), 2
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree2_chain")), 2
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree3_chain")), 2
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree1_conv")), 1
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree2_conv")), 1
+    #	)
+    # expect_equal(
+    #	length(list.files(pattern = "mulTree_testing-tree3_conv")), 1
+    #	)
     # #File remove successful
-    # expect_true(all(file.remove(list.files(pattern = "mulTree_testing"))))
+    # expect_true(
+    #	all(file.remove(list.files(pattern = "mulTree_testing")))
+    #	)
     # # Timer test
 
 })
--- tests/testthat/test-tree.bind.R
+++ tests/testthat/test-tree.bind.R
@@ -4,85 +4,147 @@ context("tree.bind")
 # Testing get.replace
 test_that("get.replace works", {
     # Only one tree and one sample (no replace)
-    expect_false(get.replace(rtree(3), 1))
+    expect_false(
+    	get.replace(rtree(3), 1)
+    	)
     # Only one tree and more samples (replace)
-    expect_true(get.replace(rtree(3), 2))
+    expect_true(
+    	get.replace(rtree(3), 2)
+    	)
     # Only one (multi)tree and one sample (no replace)
-    expect_false(get.replace(rmtree(1,3), 1))
+    expect_false(
+    	get.replace(rmtree(1,3), 1)
+    	)
     # Only one (multi)tree and one sample (no replace)
-    expect_true(get.replace(rmtree(1,3), 2))
+    expect_true(
+    	get.replace(rmtree(1,3), 2)
+    	)
     # More trees than samples (no replace)
-    expect_false(get.replace(rmtree(3,3), 2))
+    expect_false(
+    	get.replace(rmtree(3,3), 2)
+    	)
     # More samples than trees (replace)
-    expect_true(get.replace(rmtree(3,3), 4))
+    expect_true(
+    	get.replace(rmtree(3,3), 4)
+    	)
 })
 
 # Testing sample.trees
 test_that("sample.trees works", {
     # Equals 1 (no sample)
-    expect_equal(sample.trees(rmtree(1,5), 1, FALSE), 1)
+    expect_equal(
+    	sample.trees(rmtree(1,5), 1, FALSE), 1
+    	)
     # Equals 3
     set.seed(1)
-    expect_equal(sample.trees(rmtree(3,5), 1, FALSE), 3)
+    expect_equal(
+    	sample.trees(rmtree(3,5), 1, FALSE), 3
+    	)
     # Equals 3
     set.seed(1)
-    expect_equal(sample.trees(rmtree(3,5), 1, TRUE), 3)
+    expect_equal(
+    	sample.trees(rmtree(3,5), 1, TRUE), 3
+    	)
     # is length 2
-    expect_equal(length(sample.trees(rmtree(3,5), 2, TRUE)), 2)
+    expect_equal(
+    	length(sample.trees(rmtree(3,5), 2, TRUE)), 2
+    	)
     # is length 2
-    expect_equal(length(sample.trees(rmtree(3,5), 2, FALSE)), 2)
+    expect_equal(
+    	length(sample.trees(rmtree(3,5), 2, FALSE)), 2
+    	)
 })
 
 
 # Testing add.root.age
 test_that("add.root.edge works", {
     # Error
-    expect_error(add.root.edge("bla", 10))
+    expect_error(
+    	add.root.edge("bla", 10)
+    	)
     # Phylo object
-    expect_is(add.root.edge(rtree(5), 10), "phylo")
+    expect_is(
+    	add.root.edge(rtree(5), 10), "phylo"
+    	)
     # Correct root edge
     set.seed(1)
-    expect_equal(round(add.root.edge(rtree(5), 10)$root.edge, digit=4), 8.3372)
+    expect_equal(
+    	round(add.root.edge(rtree(5), 10)$root.edge, digit=4), 8.3372
+    	)
 })
 
 # Testing lapply.bind.tree
 test_that("lapply.bind.tree works", {
     # Errors:
     # element is not numeric
-    expect_error(lapply.bind.tree("a", rmtree(3,5), rmtree(3,5), c(1,2), c(1,2), 10))
+    expect_error(
+    	lapply.bind.tree("a", rmtree(3,5), rmtree(3,5), c(1,2), c(1,2), 10)
+    	)
     # one of the trees is not multiPhylo
-    expect_error(lapply.bind.tree(1, rtree(5), rmtree(3,5), c(1,2), c(1,2), 10))
+    expect_error(
+    	lapply.bind.tree(1, rtree(5), rmtree(3,5), c(1,2), c(1,2), 10)
+    	)
     # the samples are not the same size as the elements
-    expect_error(lapply.bind.tree(3, rmtree(3,5), rmtree(3,5), c(1,2), c(1,2), 10))
+    expect_error(
+    	lapply.bind.tree(3, rmtree(3,5), rmtree(3,5), c(1,2), c(1,2), 10)
+    	)
     # root age is missing
-    expect_error(lapply.bind.tree(1, rmtree(3,5), rmtree(3,5), c(1,2), c(1,2)))
+    expect_error(
+    	lapply.bind.tree(1, rmtree(3,5), rmtree(3,5), c(1,2), c(1,2))
+    	)
     # Outputs a tree
-    expect_is(lapply.bind.tree(1, rmtree(3,5), rmtree(3,5), c(1,2), c(1,2), 10), "phylo")
+    expect_is(
+    	lapply.bind.tree(1, rmtree(3,5), rmtree(3,5), c(1,2), c(1,2), 10), "phylo"
+    	)
     # Outputs has 10 tips
-    expect_equal(Ntip(lapply.bind.tree(1, rmtree(3,5), rmtree(3,5), c(1,2), c(1,2), 10)), 10)
+    expect_equal(
+    	Ntip(lapply.bind.tree(1, rmtree(3,5), rmtree(3,5), c(1,2), c(1,2), 10)), 10
+    	)
 })
 
 # Testing tree.bind
 test_that("tree.bind works", {
     # Sanitizing
     # Not a tree
-    expect_error(tree.bind("a", rtree(5), 10, 10))
+    expect_error(
+    	tree.bind("a", rtree(5), 10, 10)
+    	)
     # One tree missing
-    expect_error(tree.bind(rtree(5), 10, 10))
+    expect_error(
+    	tree.bind(rtree(5), 10, 10)
+    	)
     # Not a sample
-    expect_error(tree.bind(rmtree(3,5), rmtree(3,5), "a", 10))
+    expect_error(
+    	tree.bind(rmtree(3,5), rmtree(3,5), "a", 10)
+    	)
     # Root age is not numeric
-    expect_error(tree.bind(rtree(5), rtree(5), 3, "a"))
+    expect_error(
+    	tree.bind(rtree(5), rtree(5), 3, "a")
+    	)
     # Warning
     # same tip labels
-    expect_warning(tree.bind(rtree(5), rtree(5)))
+    expect_warning(
+    	tree.bind(rtree(5), rtree(5))
+    	)
     # Too much sample
-    expect_warning(tree.bind(rtree(5), rtree(5), sample = 3))
+    expect_warning(
+    	tree.bind(rtree(5), rtree(5), sample = 3)
+    	)
 
     # Testing
-    expect_equal(Ntip(tree.bind(rtree(5, tip.label=LETTERS[1:5]), rtree(5))), 10)
-    expect_is(tree.bind(rtree(5, tip.label=LETTERS[1:5]), rtree(5)), "phylo")
-    expect_is(tree.bind(rmtree(3,5, tip.label=LETTERS[1:5]), rmtree(3,5)), "phylo")
-    expect_is(tree.bind(rmtree(3,5, tip.label=LETTERS[1:5]), rmtree(3,5), sample = 2), "multiPhylo")
-    expect_equal(max(node.depth.edgelength(tree.bind(rmtree(3,5, tip.label=LETTERS[1:5]), rmtree(3,5), root.age = 10))), 10)
+    expect_equal(
+    	Ntip(tree.bind(rtree(5, tip.label=LETTERS[1:5]), rtree(5))), 10
+    	)
+    expect_is(
+    	tree.bind(rtree(5, tip.label=LETTERS[1:5]), rtree(5)), "phylo"
+    	)
+    expect_is(
+    	tree.bind(rmtree(3,5, tip.label=LETTERS[1:5]), rmtree(3,5)), "phylo"
+    	)
+    expect_is(
+    	tree.bind(rmtree(3,5, tip.label=LETTERS[1:5]), rmtree(3,5), sample = 2), "multiPhylo"
+    	)
+    expect_equal(
+    	max(node.depth.edgelength(tree.bind(rmtree(3,5, tip.label=LETTERS[1:5]), rmtree(3,5), root.age = 10))), 10
+    	)
 })
