8d0ff4003c5980d17470a2703f35e16640c35d54
Thomas Guillerme <guillert@tcd.ie>
2016-07-20 16:50:06 +0100

    Minor sanitising speed improvements

--- R/sanitizing.R
+++ R/sanitizing.R
@@ -2,84 +2,85 @@
 
 
 #Checking the class of an object and returning an error message if != class
-check.class<-function(object, class, msg, errorif=FALSE) {
+check.class <- function(object, class, msg, errorif = FALSE) {
     #Set msg if missing
     if(missing(msg)) {
-        msg<-paste(" must be of class: ", class, ".", sep="")
+        msg <- paste(" must be of class: ", class, ".", sep="")
     }
 
-    match_call<-match.call()
+    match_call <- match.call()
+    object_class <- class(object)
 
     #check if object is class.
     if(length(class) == 1) {
-        if(errorif==FALSE) {
-            if(class(object) != class) {
-                stop(match_call$object, msg , call.=FALSE)
+        if(errorif != TRUE) {
+            if(object_class != class) {
+                stop(match_call$object, msg , call. = FALSE)
             }
         } else {
-            if(class(object) == class) {
-                stop(match_call$object, msg , call.=FALSE)
+            if(object_class == class) {
+                stop(match_call$object, msg , call. = FALSE)
             }        
         }
     } else {
     #check if object is class in a cascade (class[1] else class[2] else class[3], etc..)
     #returns error only if object is not of any class
         for (i in 1:length(class)) {
-            if(class(object) == class[i]) {
-                class.test<-class[i]
+            if(object_class == class[i]) {
+                class.test <- class[i]
             }        
         }
         if(exists(as.character(quote(class.test)))) {
             return(class.test)
         } else {
-            stop(match_call$object, msg , call.=FALSE)
+            stop(match_call$object, msg , call. = FALSE)
         }
     }
 }
 
 
 #Checking the class of an object and returning an error message if != class
-check.length<-function(object, length, msg, errorif=FALSE) {
+check.length <- function(object, length, msg, errorif = FALSE) {
 
-    match_call<-match.call()
+    match_call <- match.call()
 
-    if(errorif==FALSE) {
+    if(errorif != TRUE) {
         if(length(object) != length) {
-            stop(match_call$object, msg , call.=FALSE)
+            stop(match_call$object, msg , call. = FALSE)
         }
     } else {
         if(length(object) == length) {
-            stop(match_call$object, msg , call.=FALSE)
+            stop(match_call$object, msg , call. = FALSE)
         }        
     }
 }
 
 
 #Cleaning a tree so that the species match with the ones in a table
-clean.tree<-function(tree, table, verbose=FALSE) {
-    missing.species<-caper::comparative.data(tree, data.frame("species"=row.names(table), "dummy"=rnorm(nrow(table)), "dumb"=rnorm(nrow(table))), "species")$dropped
+clean.tree <- function(tree, table, verbose = FALSE) {
+    missing.species <- caper::comparative.data(tree, data.frame("species" = row.names(table), "dummy" = rnorm(nrow(table)), "dumb" = rnorm(nrow(table))), "species")$dropped
     if(length(missing.species$tips) != 0) {
-        tree.tmp<-drop.tip(tree, missing.species$tips)
-        if (verbose==TRUE) {
+        tree.tmp <- drop.tip(tree, missing.species$tips)
+        if (verbose != FALSE) {
             cat("Dropped tips:\n")
             cat(missing.species$tips, sep=", ")
         }
-        tree<-tree.tmp
+        tree <- tree.tmp
     }
 
     return(tree)
 }
 
 #Cleaning a table so that the species match with the ones in the tree
-clean.table<-function(table, tree, verbose=FALSE) {
-    missing.species<-caper::comparative.data(tree, data.frame("species"=row.names(table), "dummy"=rnorm(nrow(table)), "dumb"=rnorm(nrow(table))), "species")$dropped
+clean.table <- function(table, tree, verbose = FALSE) {
+    missing.species <- caper::comparative.data(tree, data.frame("species"=row.names(table), "dummy"=rnorm(nrow(table)), "dumb"=rnorm(nrow(table))), "species")$dropped
     if(length(missing.species$unmatched.rows) != 0) {
-        table.tmp<-table[-c(match(missing.species$unmatched.rows, rownames(table))),]
-        if (verbose==TRUE) {
+        table.tmp <- table[-c(match(missing.species$unmatched.rows, rownames(table))),]
+        if (verbose != FALSE) {
             cat("Dropped rows:\n")
             cat(missing.species$unmatched.rows, sep=", ")
         }
-        table<-table.tmp
+        table <- table.tmp
     }
     return(table)
 }
